05926ccf6b9b2e6844abfbd8c9021b4229151c7a
John LaMontagne <john@lamontagne.dev>
2025-01-18 01:46:22 -0700

    updated database to include additional company fields, optimized company table

--- API/SECWatch.Domain/Features/Companies/Models/Company.cs
+++ API/SECWatch.Domain/Features/Companies/Models/Company.cs
@@ -6,7 +6,7 @@ public class Company : AggregateRoot
 {
     public string Cik { get; private set; }
     
-    public string Ticker { get; private set; }
+    public string? Ticker { get; private set; }
     
     public string Name { get; private set; }
     
@@ -15,6 +15,22 @@ public class Company : AggregateRoot
     public DateTime LastUpdated { get; private set; }
     
     public DateTime? LastKnownFilingDate { get; private set; }
+    
+    public string? EIN { get; private set; }
+    
+    public string? EntityType { get; private set; }
+    
+    public string? Website { get; private set; }
+    
+    public string? FiscalYearEnd { get; private set; }
+    
+    public string? StateOfIncorporation { get; private set; }
+    
+    public string? PhoneNumber { get; private set; }
+    
+    public string? FormerNames { get; private set; }
+    
+    public string? Exchanges { get; private set; }
     
     private List<Address> _addresses = [];
     
